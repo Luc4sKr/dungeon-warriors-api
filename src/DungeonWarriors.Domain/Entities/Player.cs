2caa1814d1e6e82d8f5deffff5cf093b0db29b20
Lucas <lucaskreuch77@gmail.com>
2023-08-10 21:58:51 -0300

    criando as entidades

---
+++ src/DungeonWarriors.Domain/Entities/Player.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DungeonWarriors.Domain.Entities
+{
+    public class Player
+    {
+        public int Id { get; set; }
+        public string username { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public DateOnly createdOn { get; set; }
+        public int MaxScore { get; set; }
+        public int Coins { get; set; }
+
+        public virtual Weapon Weapon { get; set; }
+    }
+}
